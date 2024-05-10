7f0edc8b9dace699f5c462e08eb3582742670478
William Gilpin <wgilpin@gmail.com>
2024-05-10 14:38:00 +0000

    allow 2000 char URLs

--- doofer/models.py
+++ doofer/models.py
@@ -6,11 +6,14 @@ from markdown import markdown # markdown -> html
 from markdownify import markdownify  # html -> markdown
     
 
+
 class Note(models.Model):
+    URL_MAX_LENGTH = 2000
+
     title = models.CharField(blank=True, max_length=255)
     comment = models.TextField(blank=True)
     snippet = models.TextField(blank=True)
-    url = models.URLField(blank=True)
+    url = models.URLField(blank=True, max_length=URL_MAX_LENGTH)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     related_updated_at = models.DateTimeField(auto_now=True)
@@ -20,9 +23,14 @@ class Note(models.Model):
 
     # validate model and convert html to markdown
     def clean(self) -> None:
+        # truncate url to URL_MAX_LENGTH
+        if self.url and len(self.url) > self.URL_MAX_LENGTH:
+            self.url = self.url[:self.URL_MAX_LENGTH]
+
         # need at least one of title, comment, snippet, url
         if not any([self.title, self.comment, self.snippet, self.url]):
             raise ValidationError('Need at least one of title, comment, snippet, url')
+        
         # convert html snippet to markdown
         if self.snippet:
             self.snippet = markdownify(self.snippet)
