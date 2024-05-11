23810159fdd8dc4bbe9969242636b4267ca5922c
William Gilpin <wgilpin@gmail.com>
2024-05-11 11:37:33 +0000

    show youtube thumbnails on list page

--- doofer/models.py
+++ doofer/models.py
@@ -1,8 +1,6 @@
-from typing import Collection
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.forms import ValidationError
-from markdown import markdown # markdown -> html
+from urllib.parse import urlparse, parse_qs
 from markdownify import markdownify  # html -> markdown
     
 
@@ -22,22 +20,28 @@ class Note(models.Model):
 
     # validate model and convert html to markdown
     def clean(self) -> None:
-        # truncate url to URL_MAX_LENGTH
+        """ truncate url to URL_MAX_LENGTH """
         if self.url and len(self.url) > self.URL_MAX_LENGTH:
             self.url = self.url[:self.URL_MAX_LENGTH]
 
         # need at least one of title, comment, url
         if not any([self.title, self.comment, self.url]):
             raise ValidationError('Need at least one of title, comment, url')
-        
+
         # convert any html to markdown
         if self.comment:
             self.comment = markdownify(self.comment)
 
-    # convert markdown to html for display
-    def as_html(self):
-        return markdown(self.comment)
-    
-    # convert to string
+    def get_yt_thumbnail_url(self):
+        """ get the video id from a the url if it's a youtube url """
+        if 'youtube.com' in self.url:
+            parsed_url = urlparse(self.url)
+            query_params = parse_qs(parsed_url.query)
+            video_id = query_params.get('v')
+            if video_id:
+                return f'https://img.youtube.com/vi/{video_id[0]}/sddefault.jpg'
+        return None
+
     def __str__(self):
-        return self.title
+        """ convert to string """
+        return str(self.title)
