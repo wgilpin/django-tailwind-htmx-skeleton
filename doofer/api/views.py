1df357442f4c768c436ced28dcfef5e09e222819
William Gilpin <wgilpin@gmail.com>
2024-05-19 20:29:13 +0000

    add full crud for notes api

--- doofer/api/views.py
+++ doofer/api/views.py
@@ -3,6 +3,8 @@
 # pylint: disable=no-member
 
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login, logout
@@ -12,31 +14,39 @@ from doofer.models import Note
 
 
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def get_notes(request):
     """Get all notes for current user"""
     notes = Note.objects.filter(user=request.user.id)
     serializer = NoteSerializer(notes, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=200)
 
 
-@api_view(["GET", "POST"])
+@api_view(["GET", "PUT", "DELETE"])
+@permission_classes([IsAuthenticated])
 def note_detail(request, id_):
     """Get or update a note"""
+
     try:
         note = Note.objects.get(id=id_)
     except Note.DoesNotExist:
         return Response(status=404)
+    if request.method == "DELETE":
+        note.delete()
+        return Response(status=204)
     if request.method == "GET":
         serializer = NoteSerializer(note)
         return Response(serializer.data)
+    # it's a PUT request
     serialiser = NoteSerializer(note, data=request.data)
     if serialiser.is_valid():
         serialiser.save()
-        return Response(serialiser.data, status=201)
+        return Response(serialiser.data, status=200)
     return Response(serialiser.errors, status=400)
 
 
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def note_create(request):
     """create note"""
     serialiser = NoteSerializer(data=request.data)
@@ -47,6 +57,7 @@ def note_create(request):
 
 
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def auth_login(request):
     """login the user given username and password"""
     username = request.data.get("username")
@@ -61,4 +72,18 @@ def auth_login(request):
         return Response({"message": "Login successful"}, status=200)
     else:
         # Return error response
-        return Response({"message": "Invalid credentials"}, status=401)
+        return Response({"message": "Invalid credentials!"}, status=401)
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def get_all(request):
+    """Get all notes for current user"""
+    # TODO: remove this debug code
+    print(request.path)
+    for key, value in request.headers.items():
+        print(f"{key}: {value}")
+
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
